c9bddc191d297c4fdec646bd9cfc04f86e8d8e52
SaiVidyaSri <166325469+SaiVidyaSri@users.noreply.github.com>
2025-08-11 14:39:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 // Set this to your Render backend URL after deployment
-const API_BASE = 'https://your-backend-name.onrender.com';
+const API_BASE = 'https://couriertracking-bend.onrender.com';
